df32e784f2ae02bd167743b3bc638b398260a0c1
Roberto <roberto.alberto.lira@gmail.com>
2023-03-18 13:56:41 -0600

    versión final caracter

--- JS/card.js
+++ JS/card.js
@@ -1,31 +1,10 @@
-/*
-anchor.setAttribute("href",`./view/detalles.html?id=${id}`)
-
-<div class="col-5">
-          <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="...">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-*/
-
 let createCard = (imgSrc,cardTitle,pContent,id) => {
     let divCol = document.createElement("div")
     divCol.classList.add("col-6")
 
     let divCard = document.createElement("div")
     divCard.classList.add("card","mb-3")
-    divCard.setAttribute("style","max-width: 540px;")
+    divCard.setAttribute("style","max-width: 600px;")
 
     let divRow = document.createElement("div")
     divRow.classList.add("row","g-0")
